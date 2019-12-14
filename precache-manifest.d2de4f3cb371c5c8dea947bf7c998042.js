45b147f455ff65ecd1a914dc18e2fba6ebc77e3d
kaustubhn <kaustubhnarkhede88@gmail.com>
2019-12-14 13:57:59 +0530

    Updates

---
+++ precache-manifest.d2de4f3cb371c5c8dea947bf7c998042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f122774ee5b4f5a40f54f567ec9b99",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "88c9edc1d3b538091e71",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5db30a7dc7784c533fa",
+    "url": "/hello/static/js/2.776264eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hello/static/js/2.776264eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88c9edc1d3b538091e71",
+    "url": "/hello/static/js/main.ba1f6526.chunk.js"
+  },
+  {
+    "revision": "25753f2547bc8db63bb7",
+    "url": "/hello/static/js/runtime-main.0588c8c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
